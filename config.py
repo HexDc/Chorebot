5989fd1e1e449cff5f1ed186314215ade37406ad
HexDc <41858208+HexDc@users.noreply.github.com>
2019-07-22 22:25:43 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,12 @@
-descript=""
-descr=""
-link=""
+#Message
+descript="1:시러시러잉\n2:아뇨 없어요\n3:가지말고 돌아오세요\n4:말\n5:으악 더러워\n6:도대체 왜 이러는 걸까요\n7:랜샤년아 조용히 해\n8:넌 히오스 3천시간형이나 받아라\n9:으ㅏ악\n10:난 요즘 왜 어딜가든 샌드백 포지션이냐\n11:사람이 할 말이 있지\n12:내가 저 이모티콘 생긴걸 좋아해야하나 싫어해야하나\n13:그림 잘 그리면 좋은 점 꼴리는거 스스로 그릴 수 있음 "
+#command prefix
+prefix = [
+    ';',
+    '^',
+]
+#idk this job
+description = ';clist'
+link = "https://raw.githubusercontent.com/HexDc/Chorebot/master/data/"
+#your bot's token
+token=''
